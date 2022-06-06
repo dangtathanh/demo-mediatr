7469887b49b570895623d60229be0bef0fd033af
Hanh Dang <dangtathanh@gmail.com>
2022-06-06 13:44:35 +0700

    fix: Add 1 more line

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace Demo.MediatR
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient<IGroupService, GroupService>();
             
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
